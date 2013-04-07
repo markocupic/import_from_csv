153e2df4128887ded6144ed9eee532955eecdeb3
Marko Cupic <m.cupic@gmx.ch>
2013-04-07 10:05:47 +0200

    updated language files

--- languages/de/tl_import_from_csv.php
+++ languages/de/tl_import_from_csv.php
@@ -12,8 +12,8 @@
 // fields
 $GLOBALS['TL_LANG']['tl_import_from_csv']['import_table'] = array('Datentabelle für Import auswählen', 'Wählen Sie eine Tabelle, in welche die Daten importiert werden sollen, aus.');
 $GLOBALS['TL_LANG']['tl_import_from_csv']['import_mode'] = array('Import-Modus', 'Entscheiden Sie, ob die Tabelle vor dem Import gelöscht werden soll oder die Daten an die bestehenden Eiträge angehängt werden sollen.');
-$GLOBALS['TL_LANG']['tl_import_from_csv']['field_enclosure'] = array('Felder eingeschlossen von', 'Mit welchem Zeichen sind die Felder eingeschlossen? Normalerweise ein doppeltes Anführungszeichen: => "');
-$GLOBALS['TL_LANG']['tl_import_from_csv']['field_separator'] = array('Felder getrennt von', 'Mit welchem Zeichen sind die Felder in der csv-Datei voneinander getrennt? Normalerweise ein Semikolon: => ;');
+$GLOBALS['TL_LANG']['tl_import_from_csv']['field_enclosure'] = array('Felder eingeschlossen von', 'Zeichen, von welchem die Felder in der csv-Datei eingeschlossen sind. Normalerweise ein doppeltes Anführungszeichen: => "');
+$GLOBALS['TL_LANG']['tl_import_from_csv']['field_separator'] = array('Felder getrennt von', 'Zeichen, mit dem die Felder in der csv-Datei voneinander getrennt sind. Normalerweise ein Semikolon: => ;');
 $GLOBALS['TL_LANG']['tl_import_from_csv']['selected_fields'] = array('Felder für Importvorgang auswählen.');
 $GLOBALS['TL_LANG']['tl_import_from_csv']['fileSRC'] = array('csv-Datei auswählen');
 
--- languages/en/tl_import_from_csv.php
+++ languages/en/tl_import_from_csv.php
@@ -13,13 +13,13 @@
 
 
 // fields
-$GLOBALS['TL_LANG']['tl_import_from_csv']['import_table'] = array('Import data into this table', 'Choose a table for the import');
-$GLOBALS['TL_LANG']['tl_import_from_csv']['import_mode'] = array('Import mode', 'Choose if the table will be truncated before importing the data from csv.');
-$GLOBALS['TL_LANG']['tl_import_from_csv']['field_enclosure'] = array('Field enclosure', 'Mit welchem Zeichen sind die Feldinhalte umschlossen? Normalerweise: "');
-$GLOBALS['TL_LANG']['tl_import_from_csv']['field_separator'] = array('Field separator', 'Mit welchem Zeichen sind die Felder in der Csv-Datei voneinander getrennt? Normalerweise ist das ein Semikolon: => ;');
+$GLOBALS['TL_LANG']['tl_import_from_csv']['import_table'] = array('Import data into this table', 'Choose a table for import.');
+$GLOBALS['TL_LANG']['tl_import_from_csv']['import_mode'] = array('Import mode', 'Decide if the table will be truncated before importing the data from the csv-file.');
+$GLOBALS['TL_LANG']['tl_import_from_csv']['field_enclosure'] = array('Field enclosure', 'Character with which  the field-content is enclosed. Normally it is a double quote: => "');
+$GLOBALS['TL_LANG']['tl_import_from_csv']['field_separator'] = array('Field separator', 'Character with which the fields are separated. Normally it is a semicolon: => ;');
 $GLOBALS['TL_LANG']['tl_import_from_csv']['selected_fields'] = array('Select the fields for the import');
 $GLOBALS['TL_LANG']['tl_import_from_csv']['fileSRC'] = array('Select a csv-file for the import');
 
 //references
-$GLOBALS['TL_LANG']['tl_import_from_csv']['truncate_table'] = array('truncate table before the import process');
-$GLOBALS['TL_LANG']['tl_import_from_csv']['append_entries'] = array('only append data into a given table');
+$GLOBALS['TL_LANG']['tl_import_from_csv']['truncate_table'] = array('truncate the target table before importing data');
+$GLOBALS['TL_LANG']['tl_import_from_csv']['append_entries'] = array('only append data into the target table');
