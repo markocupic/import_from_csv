68c65e1d0a7434c56c654e03ab047f1b13aa13c6
MoppieMop <moppie.mop@gmx.de>
2016-06-08 09:28:35 +0200

    Make hook callback PHP7 compatible and replace String to StringUtil for Contao 3.5 compatibility

--- classes/ImportFromCsv.php
+++ classes/ImportFromCsv.php
@@ -177,7 +177,7 @@ class ImportFromCsv extends \Backend
                     foreach ($GLOBALS['TL_HOOKS']['importFromCsv'] as $callback)
                     {
                         $this->import($callback[0]);
-                        $arrCustomValidation = $this->$callback[0]->$callback[1]($arrCustomValidation, $this);
+                        $arrCustomValidation = $this->{$callback[0]}->{$callback[1]}($arrCustomValidation, $this);
                         if(!is_array($arrCustomValidation))
                         {
                             die('Als Rückgabewert wird ein Array erwartet. Fehler in ' . __FILE__ . ' in Zeile ' . __LINE__ . '.');
@@ -359,7 +359,11 @@ class ImportFromCsv extends \Backend
                 {
                     $v = serialize($v);
                 }
-                $htmlReport .= sprintf('<tr class="%s"><td>%s</td><td>%s</td></tr>', $cssClass, \String::substr($k, 30), \String::substrHtml($v, 90));
+                if(class_exists('\Contao\StringUtil')) {
+                    $htmlReport .= sprintf('<tr class="%s"><td>%s</td><td>%s</td></tr>', $cssClass, \StringUtil::substr($k, 30), \StringUtil::substrHtml($v, 90));
+                } else {
+                    $htmlReport .= sprintf('<tr class="%s"><td>%s</td><td>%s</td></tr>', $cssClass, \String::substr($k, 30), \String::substrHtml($v, 90));
+                }
             }
 
 
